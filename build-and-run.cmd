9b9787b0403cab65e440d2b8f7dbb569f06b53b4
pedrothome1 <pedrothome2@outlook.com>
2022-02-23 23:40:42 -0300

    improve build scripts

--- build-and-run.cmd
+++ build-and-run.cmd
@@ -1,3 +1,4 @@
 @echo off
+if exist class\webserver rmdir /S /Q class\webserver
 javac -sourcepath .\src\main\java\ -classpath .\class\ -d .\class\ .\src\main\java\webserver\Program.java
 java -cp .\class\ webserver.Program "webroot"
--- build-jar.cmd
+++ build-jar.cmd
@@ -1,3 +1,4 @@
 @echo off
+if exist class\webserver rmdir /S /Q class\webserver
 javac -sourcepath .\src\main\java\ -classpath .\class\ -d .\class\ .\src\main\java\webserver\Program.java
 jar -cfe ServerProgram.jar webserver.Program -C .\class\ webserver
